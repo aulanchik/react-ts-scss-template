bc23fea5e11504a9ad0467c1e35521b940df2eb4
Artyom Ulanchik <artyom.u@outlook.com>
2023-06-18 21:26:25 +0300

    v 1.0

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App: React.FC = () => (
+  <div data-testid="app">
+    <h1>Ready</h1>
+  </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const config = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  devServer: {
+    port: 8000,
+    open: true,
+    compress: true,
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.ts(x)?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svg$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              mimetype: "image/png",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "index.html" }],
+    }),
+  ],
+};
+
+module.exports = config;
